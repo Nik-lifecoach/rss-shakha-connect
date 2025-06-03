99ac8ecc27fd7a1edc2f956241bc0ca3d34acbba
Nik-lifecoach <nik.lifecoach@gmail.com>
2025-06-03 23:00:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,4 +48,7 @@ def get_location(id):
     })
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # ✅ Auto-creates the DB if it doesn't exist
     app.run(debug=True)
+
